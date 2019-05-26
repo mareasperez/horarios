e42f0cab3848a37ee0996364342f334f2432ec86
maury <mareasperez7@gmail.com>
2019-05-26 00:48:29 -0600

    crud completo facultad

--- apps/facultades/urls.py
+++ apps/facultades/urls.py
@@ -5,8 +5,7 @@ from apps.facultades.views import FacultadListView, Facultadone
 from rest_framework.request import Request
 app_name = "facultades"
 urlpatterns = [
-    path('facultades/',FacultadListView.as_view()),
-    path('facultades/detail/<int:pk>',Facultadone.as_view()),
+    path('facultades/',Facultadone.as_view()),
     path('facultades/<int:pk>', FacultadListView.as_view())
 ]
 
--- apps/facultades/views.py
+++ apps/facultades/views.py
@@ -6,7 +6,30 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+
 class FacultadListView(APIView):
+    def get(self, request, pk):
+        facultades = Facultades.objects.get(facultad_id=pk)
+        serializer = FacultadSerializer(facultades)
+        return Response({"facultad": serializer.data})
+
+    def put(self, request, pk):
+        saved_facultad = get_object_or_404(
+            Facultades.objects.all(), facultad_id=pk)
+        facultad = request.data.get('facultad')
+        serializer = FacultadSerializer(
+            instance=saved_facultad, data=facultad, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            facultad_saved = serializer.save()
+        return Response({"success": "Facultad '{}' updated successfully".format(facultad_saved.facultad_nombre)})
+
+    def delete(self, request, pk):
+        facultad = get_object_or_404(Facultades.objects.all(), facultad_id=pk)
+        facultad.delete()
+        return Response({"message": "Facultad with id `{}` has been deleted.".format(pk)}, status=204)
+
+
+class Facultadone(APIView):
     def get(self, request):
         facultades = Facultades.objects.all()
         serializer = FacultadSerializer(facultades, many=True)
@@ -18,21 +41,3 @@ class FacultadListView(APIView):
         if serializer.is_valid(raise_exception=True):
             facultad_saved = serializer.save()
         return Response({"success": "Facultad: '{}' creada satisfactoriamente".format(facultad_saved.facultad_nombre)})
-    
-    def put(self, request, pk):
-        saved_facultad = get_object_or_404(Facultades.objects.all(), pk=pk)
-        facultad = request.data.get('facultad')
-        serializer = FacultadSerializer(instance=saved_facultad, data=facultad, partial=True)
-        if serializer.is_valid(raise_exception=True):
-            facultad_saved = serializer.save()
-        return Response({"success": "Facultad '{}' updated successfully".format(facultad_saved.facultad_nombre)})
-    
-    def delete(self, request, pk):
-        facultad = get_object_or_404(Facultades.objects.all(), pk=pk)
-        facultad.delete()
-        return Response({"message": "Facultad with id `{}` has been deleted.".format(pk)},status=204)
-class Facultadone(APIView):
-    def get(self, request, pk):
-        facultades = Facultades.objects.get(facultad_id=pk)
-        serializer = FacultadSerializer(facultades)
-        return Response({"facultad": serializer.data})
