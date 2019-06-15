70e89528259021e0eb39fe51bff7c8923e82f20e
mareasperez7 <mareasperez7@gmail.com>
2019-06-14 21:20:55 -0600

    carrera lista

--- apps/carreras/serializers.py
+++ apps/carreras/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Carrera
-from apps.facultades.models import Facultad
+from apps.departamento.models import Departamento
 
 class CarreraSerializer(serializers.Serializer):
     # carrera_nombre = models.CharField(max_length=50)
@@ -8,7 +8,7 @@ class CarreraSerializer(serializers.Serializer):
     # carrera_facultad = models.ForeignKey(Facultad, on_delete=models.CASCADE)
     carrera_nombre = serializers.CharField(max_length=50)
     carrera_id = serializers.IntegerField()
-    carrera_facultad = serializers.PrimaryKeyRelatedField(queryset=Facultad.objects.all())
+    carrera_departamento = serializers.PrimaryKeyRelatedField(queryset=Departamento.objects.all())
 
     def create(self, validated_data):
         return Carrera.objects.create(**validated_data)
@@ -16,6 +16,6 @@ class CarreraSerializer(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.carrera_nombre= validated_data.get('carrera_nombre', instance.carrera_nombre)
         instance.carrera_id = validated_data.get('carrera_id', instance.carrera_id)
-        instance.carrera_facultad = validated_data.get('carrera_facultad',instance.carrera_facultad)
+        instance.carrera_departamento = validated_data.get('carrera_departamento',instance.carrera_departamento)
         instance.save()
         return instance
