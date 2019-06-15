36805d74b120a4d5d37c2af61cb0468fc4a97c54
mareasperez7 <mareasperez7@gmail.com>
2019-06-15 14:49:42 -0600

    pde listo

---
+++ apps/componentes/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from .models import Clase
+from apps.carreras.models import Carrera
+from apps.ciclos.models import Ciclo
+
+class ClaseSerializer(serializers.Serializer):
+    # clase_nombre = models.CharField(max_length=50)
+    # clase_id = models.IntegerField(primary_key=True)
+    # clase_tipo = models.IntegerField(default=0)
+    # clase_ciclo = models.ForeignKey(Ciclo, on_delete=models.CASCADE)
+    # clase_carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+    
+    clase_nombre = serializers.CharField(max_length=50)
+    clase_id = serializers.IntegerField()
+    clase_tipo = serializers.IntegerField()
+    clase_ciclo = serializers.PrimaryKeyRelatedField(queryset=Ciclo.objects.all())
+    clase_carrera = serializers.PrimaryKeyRelatedField(queryset=Carrera.objects.all())
+
+    def create(self, validated_data):
+        return Clase.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.clase_nombre= validated_data.get('clase_nombre', instance.clase_nombre)
+        instance.clase_id = validated_data.get('clase_id', instance.clase_id)
+        instance.clase_tipo = validated_data.get('clase_tipo', instance.clase_tipo)
+        instance.clase_ciclo = validated_data.get('clase_ciclo', instance.clase_ciclo)
+        instance.clase_carrera = validated_data.get('clase_carrera',instance.clase_carrera)
+        instance.save()
+        return instance
