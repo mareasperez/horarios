026401e4023e426a35c02c82f3f97e0af552b5dd
mareasperez7 <mareasperez7@gmail.com>
2019-06-15 17:32:59 -0600

    planificacion listo v2

--- apps/docente_area/urls.py
+++ apps/docente_area/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from .views import DocenteArea
-app_name = "departamento"
+app_name = "docenteArea"
 urlpatterns = [
     path('',DepartamentoSinArg.as_view()),
     path('<int:pk>', DepartamentoConArgumento.as_view())
