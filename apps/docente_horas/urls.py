ea0f09ae3bc7ae2e47b01a3dbf8881e324f62913
mareasperez7 <mareasperez7@gmail.com>
2019-06-16 14:20:46 -0600

    docenteareaFix

--- apps/docente_horas/urls.py
+++ apps/docente_horas/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from .views import DocenteHorasConArgumento,DocenteHorasSinArg
-app_name = "docenteArea"
+app_name = "docentehoras"
 urlpatterns = [
     path('',DocenteHorasSinArg.as_view()),
     path('<int:pk>', DocenteHorasConArgumento.as_view())
