9dfce029ad3fbde8404d74a3ab4b0700beb29e36
Maury Areas <mareasperez7@gmail.com>
2019-07-17 00:11:37 -0600

    fix import app.py websocket

--- apps/websocket/apps.py
+++ apps/websocket/apps.py
@@ -7,3 +7,16 @@ class WebsocketConfig(AppConfig):
     def ready(self):
         pass
         #from . import signals
+        from .signal import area_signals
+        from .signal import aula_signals
+        from .signal import carrera_signals
+        from .signal import componente_signals
+        from .signal import docente_signals
+        from .signal import docenteArea_signals
+        from .signal import docenteHora_signals
+        from .signal import facultad_signals
+        from .signal import grupo_signals
+        from .signal import horario_signals
+        from .signal import planDeEstudio_signals
+        from .signal import planificacion_signals
+        from .signal import recinto_signals
