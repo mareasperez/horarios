3674671ff0c1670abd37629408d8ed1d4f2128da
Maury Areas <mareasperez7@gmail.com>
2019-07-16 23:16:06 -0600

    auth en facultades y clean imports

---
+++ apps/autenticacion/views.py
@@ -0,0 +1,21 @@
+
+from django.contrib.auth import authenticate
+from rest_framework import permissions
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+...
+
+class LoginView(APIView):
+    permission_classes = [permissions.AllowAny, ]
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+
+        user = authenticate(username=data.get('username'), password=data.get('password'))
+        print (user)
+        if user:
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=200)
+        return Response(status=400)
