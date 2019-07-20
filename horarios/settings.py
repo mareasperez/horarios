72dcd3e092af7fd41f13f7dcecd325bbdfd9367b
maury <mareasperez7@gmail.com>
2019-07-19 18:27:52 -0600

    migrate to postgrsql

--- horarios/settings.py
+++ horarios/settings.py
@@ -123,11 +123,14 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'horariosdb',
+        'USER': 'usuario',
+        'PASSWORD': 'noquiero',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
