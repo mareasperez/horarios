eaa9518b07bbc3f8b5ed9342006cd7714d6cbf6b
marea <88530749Mjap>
2019-08-21 18:19:07 -0600

    fix autonumber pde

--- apps/plan_de_estudio/models.py
+++ apps/plan_de_estudio/models.py
@@ -9,7 +9,7 @@ YEAR_CHOICES = []
 for r in range(2000, (datetime.datetime.now().year+2)):
     YEAR_CHOICES.append((r,r))
 class PlanDeEstudio(models.Model):
-    pde_id = models.IntegerField(('id'),primary_key=True)
+    pde_id = models.AutoField('id',primary_key=True)
     pde_nombre = models.CharField('nombre',max_length=50)
     pde_anyo = models.IntegerField('año', choices=YEAR_CHOICES,default=datetime.datetime.now().year)
     pde_carrera = models.ForeignKey(Carrera,on_delete=models.CASCADE,null=True)
