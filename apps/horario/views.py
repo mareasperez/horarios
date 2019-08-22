383e5799d2cd5661bd9c6535b0aafdb44285fdfa
marea <88530749Mjap>
2019-08-21 18:10:57 -0600

    fix horrario w/o auth

--- apps/horario/views.py
+++ apps/horario/views.py
@@ -10,6 +10,8 @@ from .serializers import HorarioSerializer
 
 
 class HorarioAll(APIView):
+    authentication_classes = (JSONWebTokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     def get(self, request):
         horario = Horario.objects.all()
         serializer = HorarioSerializer(horario, many=True)
@@ -24,6 +26,8 @@ class HorarioAll(APIView):
 
 
 class HorarioByID(APIView):
+    authentication_classes = (JSONWebTokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     def get(self, request, pk):
         try:
             horario = Horario.objects.get(horario_id=pk)
