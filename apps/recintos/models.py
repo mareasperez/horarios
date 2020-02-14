1c0cfa0e3bb5dc8b60fa06fb8df2bd64d12af8d6
maury <mareasperez7@gmail.com>
2020-02-14 01:07:09 -0600

    format code: full recintos

--- apps/recintos/models.py
+++ apps/recintos/models.py
@@ -9,7 +9,8 @@ class Recinto(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     recinto_nombre = models.CharField(max_length=50)
     recinto_id = models.AutoField(primary_key=True)
-    recinto_ubicacion = models.CharField(max_length=250,default='sin ubicacion')
-    recinto_facultad = models.ForeignKey(Facultad,on_delete=models.CASCADE)
+    recinto_ubicacion = models.CharField(max_length=250, default='sin ubicacion')
+    recinto_facultad = models.ForeignKey(Facultad, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.recinto_nombre
--- apps/recintos/urls.py
+++ apps/recintos/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
-from apps.recintos.views import RecintoConArgumento,RecintoSinArg, RecintoMixed
-from django.urls import path
 
 from apps.recintos.views import RecintoConArgumento, RecintoSinArg
+from apps.recintos.views import RecintoMixed
 
 app_name = "recintos"
 urlpatterns = [
-    path('',RecintoSinArg.as_view()),
+    path('', RecintoSinArg.as_view()),
     path('<int:pk>', RecintoConArgumento.as_view()),
     path('<str:clave>=<str:value>', RecintoMixed.as_view()),
 ]
-
-
