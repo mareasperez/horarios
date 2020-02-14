9438823a39c38d120c8c560798fe8492c88780d6
maury <mareasperez7@gmail.com>
2020-02-14 00:40:22 -0600

    format code: full docente_area

--- apps/docente_area/models.py
+++ apps/docente_area/models.py
@@ -9,11 +9,13 @@ class DocenteArea(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     da_id = models.AutoField(primary_key=True)
-    da_area = models.ForeignKey(Area,on_delete=models.CASCADE)
-    da_docente = models.ForeignKey(Docente,on_delete=models.CASCADE)
+    da_area = models.ForeignKey(Area, on_delete=models.CASCADE)
+    da_docente = models.ForeignKey(Docente, on_delete=models.CASCADE)
+
     class Meta:
         verbose_name = "Docente Area"
         verbose_name_plural = "Areas de los Docentes"
         ordering = ["-da_id"]  # <=====
+
     def __str__(self):
-        return "%s %s"%(self.da_id,self.da_docente)
+        return "%s %s" % (self.da_id, self.da_docente)
