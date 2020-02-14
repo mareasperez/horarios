ceec0979796cbc351a107440745f396d165f8f4e
maury <mareasperez7@gmail.com>
2020-02-14 00:30:55 -0600

    format code: full carreras

--- apps/carreras/models.py
+++ apps/carreras/models.py
@@ -2,14 +2,17 @@ from django.db import models
 
 from apps.departamento.models import Departamento
 
-carrera_tipo_choice =(("P",'Presencial'),("V",'Virtual'))
+carrera_tipo_choice = (("P", 'Presencial'), ("V", 'Virtual'))
+
+
 # Create your models here.
 class Carrera(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     carrera_nombre = models.CharField(max_length=50)
-    carrera_tipo = models.CharField(choices=carrera_tipo_choice,default='P',null=False,max_length=1)
+    carrera_tipo = models.CharField(choices=carrera_tipo_choice, default='P', null=False, max_length=1)
     carrera_id = models.AutoField(primary_key=True)
-    carrera_departamento = models.ForeignKey(Departamento,on_delete=models.CASCADE)
+    carrera_departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.carrera_nombre
--- apps/carreras/serializers.py
+++ apps/carreras/serializers.py
@@ -1,24 +1,23 @@
 from rest_framework import serializers
 
-from apps.departamento.models import Departamento
 from .models import Carrera
 
 
 class CarreraSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Carrera
-            fields = ('carrera_nombre', 'carrera_id', 'carrera_departamento', 'created_at', 'updated_at')
+    class Meta:
+        model = Carrera
+        fields = ('carrera_nombre', 'carrera_id', 'carrera_departamento', 'created_at', 'updated_at')
 
-    # carrera_nombre = serializers.CharField(max_length=50)
-    # carrera_id = serializers.IntegerField(allow_null=True)
-    # carrera_departamento = serializers.PrimaryKeyRelatedField(queryset=Departamento.objects.all())
-    #
-    # def create(self, validated_data):
-    #     return Carrera.objects.create(**validated_data)
-    #
-    # def update(self, instance, validated_data):
-    #     instance.carrera_nombre= validated_data.get('carrera_nombre', instance.carrera_nombre)
-    #     #instance._id* = validated_data.get('carrera_id', instance.carrera_id)
-    #     instance.carrera_departamento = validated_data.get('carrera_departamento',instance.carrera_departamento)
-    #     instance.save()
-    #     return instance
+# carrera_nombre = serializers.CharField(max_length=50)
+# carrera_id = serializers.IntegerField(allow_null=True)
+# carrera_departamento = serializers.PrimaryKeyRelatedField(queryset=Departamento.objects.all())
+#
+# def create(self, validated_data):
+#     return Carrera.objects.create(**validated_data)
+#
+# def update(self, instance, validated_data):
+#     instance.carrera_nombre= validated_data.get('carrera_nombre', instance.carrera_nombre)
+#     #instance._id* = validated_data.get('carrera_id', instance.carrera_id)
+#     instance.carrera_departamento = validated_data.get('carrera_departamento',instance.carrera_departamento)
+#     instance.save()
+#     return instance
