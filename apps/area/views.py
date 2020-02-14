2a4b6d59926e5e072354a1973164f2235da55c0a
maury <mareasperez7@gmail.com>
2020-02-14 00:09:57 -0600

    format code area y docente area

--- apps/area/views.py
+++ apps/area/views.py
@@ -16,9 +16,9 @@ class AreaListView(APIView):
         try:
             areaes = Area.objects.get(area_id=pk)
             serializer = AreaSerializer(areaes)
-            return Response({"area": serializer.data})
+            return Response(dict(area=serializer.data))
         except:
-            return Response({"Detail": "not found"})
+            return Response(dict(Detail="not found"))
 
     def put(self, request, pk):
         saved_area = get_object_or_404(
@@ -28,12 +28,12 @@ class AreaListView(APIView):
             instance=saved_area, data=area, partial=True)
         if serializer.is_valid(raise_exception=True):
             area_saved = serializer.save()
-        return Response({"success": "Area '{}' updated successfully".format(area_saved.area_nombre)})
+        return Response(dict(success="Area '{}' updated successfully".format(area_saved.area_nombre)))
 
     def delete(self, request, pk):
         area = get_object_or_404(Area.objects.all(), area_id=pk)
         area.delete()
-        return Response({"message": "Area with id `{}` has been deleted.".format(pk)}, status=204)
+        return Response(dict(message="Area with id `{}` has been deleted.".format(pk)), status=204)
 
 
 class Areaone(APIView):
@@ -44,9 +44,9 @@ class Areaone(APIView):
         try:
             areaes = Area.objects.all()
             serializer = AreaSerializer(areaes, many=True)
-            return Response({"area": serializer.data})
+            return Response(dict(area=serializer.data))
         except:
-            return Response({"Detail": "not found"})
+            return Response(dict(Detail="not found"))
 
     def post(self, request):
         area = request.data.get('area')
