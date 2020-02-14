283b2e7ddc6fb2d46ca9b1b69a53145e5d428232
maury <mareasperez7@gmail.com>
2020-02-14 00:38:58 -0600

    format code: full departamento

--- apps/departamento/models.py
+++ apps/departamento/models.py
@@ -11,6 +11,7 @@ class Departamento(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     departamento_nombre = models.CharField(max_length=50)
     departamento_id = models.AutoField(primary_key=True)
-    departamento_facultad = models.ForeignKey(Facultad,on_delete=models.CASCADE)
+    departamento_facultad = models.ForeignKey(Facultad, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.departamento_nombre
--- apps/departamento/urls.py
+++ apps/departamento/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-from django.urls import path
 
-from .views import DepartamentoConArgumento, DepartamentoSinArg,DepartamentoMixed
+from .views import DepartamentoConArgumento, DepartamentoSinArg, DepartamentoMixed
 
 app_name = "departamento"
 urlpatterns = [
-    path('',DepartamentoSinArg.as_view()),
+    path('', DepartamentoSinArg.as_view()),
     path('<int:pk>', DepartamentoConArgumento.as_view()),
-    path('<str:clave>=<str:value>',DepartamentoMixed.as_view())
+    path('<str:clave>=<str:value>', DepartamentoMixed.as_view())
 ]
-
-
